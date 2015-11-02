e23f13058dbb75c65dfa6967522d498fed061542
Richard G <richard@r3software.org>
2015-11-02 14:11:48 -0800

    Initial push.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject csv-emailer "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Tool for emailing promo codes from a CSV file."
+  :url "https://github.com/RGrun/csv-emailer"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
